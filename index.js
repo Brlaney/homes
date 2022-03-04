ccef81cec05087ed50a7c477b4f454f079c00cb0
brlaney42 <brlaney@outlook.com>
2022-03-04 09:40:58 -0600

    Changing project to homes

---
+++ index.js
@@ -0,0 +1,28 @@
+// src/index.ts
+// const fs = require('fs');
+const puppeteer = require('puppeteer');
+
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 30,
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
+  const page = await browser.newPage();
+  
+  await page.goto('https://www.zillow.com/', {
+    waitUntil: 'load'
+  });
+  
+  await page.click('#search-box-input');
+  await page.type('#search-box-input', 'Cookeville, TN');
+  await page.keyboard.press('Enter', { delayed: 10 });
+
+  await page.waitFor(5000);
+  
+
+  
+  await page.screenshot({ path: 'amazing.png' })
+
+})();
--- lib/endpoints/index.ts
+++
@@ -1,2 +0,0 @@
-export const BASE_URL = 'https://www.severeweatheroutlook.com/';
-export const Z_URL = 'https://www.zillow.com/homes/38501_rb/';
